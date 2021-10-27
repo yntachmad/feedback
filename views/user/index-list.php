cd3ab472c54dd88bc8258584be361fee8f500a82
yntachmad <achmad.t.jayanto@gmail.com>
2021-10-27 15:11:16 +0700

    tambah index-list

---
+++ views/user/index-list.php
@@ -0,0 +1,120 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\GridView;
+use yii\widgets\ListView;
+use yii\widgets\Pjax;
+use yii\bootstrap4\LinkPager;
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\UserSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = Yii::t('app', 'Users');
+$this->params['breadcrumbs'][] = $this->title;
+$this->params['menu']='user';
+?>
+
+
+
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Data User</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active">Contacts</li>
+                </ol>
+            </div>
+        </div>
+    </div><!-- /.container-fluid -->
+</section>
+
+<!-- Main content -->
+<section class="content">
+
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Add</h3>
+
+            <div class="card-tools">
+                <form action="<?= Url::to(['/user/search']); ?>">
+                    <div class="input-group input-group-sm" style="width: 150px;">
+
+                        <input type="text" value="<?= Yii::$app->request->get('keyword') ?>" name="keyword"
+                            class="form-control float-right" placeholder="Search">
+
+                        <div class="input-group-append">
+                            <button type="submit" class="btn btn-default">
+                                <i class="fas fa-search"></i>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body table-responsive p-2" style="height: 330px;">
+            <?php Pjax::begin(); ?>
+            <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+            <?= ListView::widget([
+                'dataProvider' => $dataProvider,
+                'itemView' => '_userItem',
+                'layout' => '<div class="d-flex flex-wrap">{items}</div>',
+                'itemOptions' => [
+                    'tag' => false,
+                ]
+            
+            ]); ?>
+
+            <?php Pjax::end(); ?>
+
+        </div>
+        <!-- /.card-body -->
+    </div>
+
+
+    <!-- /.card-body -->
+    <div class="card-footer">
+
+        <?php
+
+                      echo LinkPager::widget([
+                          'pagination' => $dataProvider->pagination,
+                          'maxButtonCount'=>4,
+                        //  'prevPageLabel' => false,
+                         // 'nextPageLabel' => false,
+                        //  'firstPageCssClass'=> 'First',
+                          'options' =>['class' =>'Contacts Page Navigation' ],
+                          'listOptions' =>['class'=>'pagination justify-content-center m-0'],
+                          'linkOptions' =>['class '=>'page-link'],
+                      ]);
+
+
+
+?>
+
+        <!-- <nav aria-label="Contacts Page Navigation">
+            <ul class="pagination justify-content-center m-0">
+                <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item"><a class="page-link" href="#">4</a></li>
+                <li class="page-item"><a class="page-link" href="#">5</a></li>
+                <li class="page-item"><a class="page-link" href="#">6</a></li>
+                <li class="page-item"><a class="page-link" href="#">7</a></li>
+                <li class="page-item"><a class="page-link" href="#">8</a></li>
+            </ul>
+        </nav> -->
+    </div>
+    <!-- /.card-footer -->
+    </div>
+    <!-- /.card -->
+
+</section>
+<!-- /.content -->
